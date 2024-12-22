851ca37d900dec172dda4e5c1a7028c6c8f25889
minindu-alwis <minindualwis@gmail.com>
2024-12-22 10:03:25 +0530

    first committt

---
+++ ItemForm-ADD-DELETE-VIEW/src/main/java/org/example/Controllers/HomePage.java
@@ -0,0 +1,22 @@
+package org.example.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HomePage {
+    public void addItemOnAction(ActionEvent actionEvent) {
+
+        Stage stage=new Stage();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/addItem.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+
+    }
+}
